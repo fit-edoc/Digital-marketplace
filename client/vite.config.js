a42d44cb240f55521efaa0387fdb2cb15dd0ad16
fitman <fitmanverma@gmail.com>
2025-10-28 17:20:03 +0530

    deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"https://github.com/fit-edoc/Digital-marketplace",
   
-  server:{
-    open:true,
-    proxy:" https://af8180b766e5.ngrok-free.app",
-    allowedHosts:true
-  }
+  // server:{
+  //   open:true,
+  //   proxy:" https://af8180b766e5.ngrok-free.app",
+  //   allowedHosts:true
+  // }
 })
